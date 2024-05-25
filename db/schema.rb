1aa0d662e370b65655041c9bdfbbcf898d5ddfc2
sohilaehab25 <sohailaehab25@gmail.com>
2024-05-25 12:41:17 +0300

    make cruds on posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_25_093237) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_25_093856) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.date "DOB"
@@ -18,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_25_093237) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
